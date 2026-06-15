b9326bbfca4b7fc02e3c4e8bb908257539ce765e
Tu2129 <tunmisemajaro001@gmail.com>
2026-06-15 04:38:17 +0100

    added carts and locations in the script.js

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
-console.log("I'm a Winner");
-console.log(8 * 8);
-function greetUser(name) {
-  const male = "Tunmise";
-  const female = "Boluwatife";
-  const joints = male + " and " + female + " are students of " + name;
-  console.log(joints);
-}
-greetUser("LASU");
-const friends = ["Seyi", "Tunmise", "Monarch", "Stephen"];
-friends.push("Boluwatife");
-console.log(friends);
-
-const clicks = ["Kunle", "Bimbo", "Bolaji", "Eve"];
-const game = clicks.pop();
-console.log(game);
-
-const time = ["Lagos", "Abuja", "Port Harcourt", "Imo"];
-for (let i = 0; i < time.length; i++) {
-  console.log(time[i]);
+const PAYSTACK_KEY = "pk_test_b8caca04e47c14d7e98e5ef05e5ac86d5e4e15aa";
+const lgaData = {
+  Lagos: [
+    "Ikeja",
+    "Lagos Island",
+    "Surulere",
+    "Lekki",
+    "Alimosho",
+    "Mushin",
+    "Oshodi",
+    "Badagry",
+    "Epe",
+    "Ikorodu",
+  ],
+  Abuja: ["Abuja Municipal", "Bwari", "Gwagwalada", "Kuje", "Kwali", "Abaji"],
+  Rivers: [
+    "Port Harcourt",
+    "Obio-Akpor",
+    "Eleme",
+    "Ikwerre",
+    "Etche",
+    "Oyigbo",
+    "Tai",
+  ],
+  Kano: [
+    "Kano Municipal",
+    "Fagge",
+    "Dala",
+    "Gwale",
+    "Tarauni",
+    "Nassarawa",
+    "Ungogo",
+  ],
+  Oyo: ["Ibadan North", "Ibadan South", "Ogbomosho", "Oyo", "Iseyin", "Saki"],
+  Delta: ["Warri", "Sapele", "Asaba", "Ughelli", "Effurun", "Agbor"],
+  Anambra: ["Awka", "Onitsha", "Nnewi", "Ekwulobia", "Aguata", "Idemili"],
+  Enugu: [
+    "Enugu North",
+    "Enugu South",
+    "Igbo-Eze",
+    "Nkanu",
+    "Udi",
+    "Igbo-Etiti",
+  ],
+  Kaduna: ["Kaduna North", "Kaduna South", "Zaria", "Kafanchan", "Kagoro"],
+  Imo: ["Owerri", "Orlu", "Okigwe", "Mbaise", "Mbano", "Ngor Okpala"],
+  Ogun: ["Abeokuta", "Sagamu", "Ijebu Ode", "Ota", "Ilaro", "Shagamu"],
+  Edo: ["Benin City", "Ekpoma", "Auchi", "Uromi", "Esan", "Igueben"],
+};
+let cart = [];
+let deliveryFee = 0;
+function addToCart(btn, name, price) {
+  let existing = cart.find((i) => i.name === name);
+  if (existing) {
+    existing.qty++;
+  } else {
+    cart.push({ name, price, qty: 1 });
+  }
+  btn.textContent = "Added";
+  btn.classList.add("added");
 }
