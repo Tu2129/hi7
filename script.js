2445193721c40c75c45eeb0abb1b0dbc66ac4ef6
Tu2129 <tunmisemajaro001@gmail.com>
2026-06-15 01:56:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("I'm a Winner");
+console.log(8 * 8);
+function greetUser(name) {
+  const male = "Tunmise";
+  const female = "Boluwatife";
+  const joints = male + " and " + female + " are students of " + name;
+  console.log(joints);
+}
+greetUser("LASU");
+const friends = ["Seyi", "Tunmise", "Monarch", "Stephen"];
+friends.push("Boluwatife");
+console.log(friends);
+
+const clicks = ["Kunle", "Bimbo", "Bolaji", "Eve"];
+const game = clicks.pop();
+console.log(game);
+
+const time = ["Lagos", "Abuja", "Port Harcourt", "Imo"];
+for (let i = 0; i < time.length; i++) {
+  console.log(time[i]);
+}
